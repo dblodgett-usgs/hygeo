e67460ca6fa0e48354340f3ccb8847a447772855
David Blodgett <dblodgett@usgs.gov>
2020-07-01 20:56:10 -0500

    get_hydrologic_location fixes #16

---
+++ R/get_hyl.R
@@ -0,0 +1,44 @@
+#' get hydrologic location
+#' @description given a set of points with a known mainstem, calculates which incremental
+#' flowpath feature each point is on including a measure along the feature.
+#' 100 measure is the top, 0 measure is the bottom.
+#' @param hyl a set of hydrologic location geometry with an identifier in the first column,
+#' the mainstem id in the seccond column, and an sf_column in the third.
+#' @param waterbody the waterbody table of an hygeo object.
+#' @importFrom nhdplusTools get_flowline_index
+#' @importFrom dplyr bind_rows select rename mutate
+#' @return sf data.frame
+#' @export
+#'
+get_hydrologic_locaton <- function(hyl, waterbody) {
+
+  if(st_crs(hyl) != st_crs(waterbody)) stop("crs must be equal")
+
+  waterbody <- waterbody %>%
+    rename(COMID = ID) %>%
+    mutate(REACHCODE = COMID,
+           FromMeas = 0, ToMeas = 100)
+
+  lps <- unique(waterbody$main_id)
+  lps <- lps[lps %in% hyl$main_id]
+
+  lapply(lps,
+         link_by_path,
+         waterbody = waterbody,
+         hyl = hyl) %>%
+    bind_rows() %>%
+    select(-REACHCODE) %>%
+    rename(ID = COMID, measure = REACH_meas)
+}
+
+link_by_path <- function(lp, hyl, waterbody, radius = 1000) {
+  if(nrow(hyl) == 0) browser()
+
+  waterbody <- waterbody[waterbody$main_id == lp, ]
+
+  hyl <- hyl[hyl$main_id == lp, ]
+
+  st_sf(bind_cols(hyl[, 1],
+                  nhdplusTools::get_flowline_index(waterbody, hyl, search_radius = radius)))
+}
+
---
+++ tests/testthat/test_id_mappers.R
@@ -0,0 +1,46 @@
+context("get_hydrologic_location")
+
+test_that("get_hydrologic_location integration test", {
+  hl <- get_test_hygoeo_object()
+  fline <- hl$fline
+  catchment <- hl$cat
+  hl <- hl$hl
+
+  src_gpkg <- system.file("gpkg/sugar_creek_fort_mill.gpkg", package = "hygeo")
+
+  mr_lp <- read_sf(src_gpkg, "NHDFlowline_Network") %>%
+    st_drop_geometry() %>%
+    align_nhdplus_names() %>%
+    select(COMID, LevelPathI)
+
+  main_id_xwalk <- select(st_drop_geometry(hl$waterbody),
+                          local_id = ID, main_id) %>%
+    left_join(get_nhd_crosswalk(fline), by = "local_id") %>%
+    mutate(COMID = as.integer(COMID)) %>%
+    select(-local_id) %>%
+    distinct() %>%
+    left_join(mr_lp, by = "COMID") %>%
+    select(-COMID)
+
+  hr_nodes <- read_sf(src_gpkg, "hr_NHDPlusFlowline") %>%
+    select(NHDPlusID = COMID, LevelPathI, Hydroseq, mr_LevelPathI)
+
+  hr_nodes <- sf::st_sf(sf::st_drop_geometry(hr_nodes),
+                        geom =  st_geometry(nhdplusTools::get_node(hr_nodes))) %>%
+    left_join(main_id_xwalk, by = c("mr_LevelPathI" = "LevelPathI"))
+
+  hyl <- select(hr_nodes, NHDPlusID, main_id)
+  waterbody <- hl$waterbody
+
+  hyl <- st_transform(hyl, 5070)
+
+  expect_error(get_hydrologic_locaton(hyl, waterbody), "crs must be equal")
+
+  waterbody <- st_transform(waterbody, 5070)
+
+  hyl_out <- get_hydrologic_locaton(hyl, waterbody)
+
+  expect_equal(nrow(hyl_out), 5053)
+
+  expect_equal(names(hyl_out), c("NHDPlusID", "ID", "measure", "offset", "geom"))
+})
